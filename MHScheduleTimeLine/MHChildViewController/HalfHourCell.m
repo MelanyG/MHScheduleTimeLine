f0d21a21f8a3f49ddb4faef197f8d46db87728f9
Melaniia Hulianovych <mhul@softserveinc.com>
2016-11-17 18:04:56 +0200

    implemented layOut for timeLine items

---
+++ MHScheduleTimeLine/MHChildViewController/HalfHourCell.m
@@ -0,0 +1,18 @@
+//
+//  HalfHourCell.m
+//  MHScheduleTimeLine
+//
+//  Created by Melaniia Hulianovych on 11/17/16.
+//  Copyright © 2016 Melaniia Hulianovych. All rights reserved.
+//
+
+#import "HalfHourCell.h"
+
+@implementation HalfHourCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
