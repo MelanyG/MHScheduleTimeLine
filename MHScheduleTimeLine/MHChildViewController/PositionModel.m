a6b1cdf035b857b1bf3d277a4574255195e3188e
Melaniia Hulianovych <mhul@softserveinc.com>
2016-11-17 19:07:57 +0200

    start implementing splitting timeline

---
+++ MHScheduleTimeLine/MHChildViewController/PositionModel.m
@@ -0,0 +1,13 @@
+//
+//  PositionModel.m
+//  MHScheduleTimeLine
+//
+//  Created by Melaniia Hulianovych on 11/17/16.
+//  Copyright © 2016 Melaniia Hulianovych. All rights reserved.
+//
+
+#import "PositionModel.h"
+
+@implementation PositionModel
+
+@end
